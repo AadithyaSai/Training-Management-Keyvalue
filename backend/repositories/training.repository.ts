8940278d0abc12b5132930f7fc5773a44228d985
NamrithaGirish <namrithac1723@gmail.com>
2025-06-20 23:48:06 +0530

    Updated admin dashboard and session update

--- backend/repositories/training.repository.ts
+++ backend/repositories/training.repository.ts
@@ -1,94 +1,91 @@
 import { Repository } from "typeorm";
 import { Training } from "../entities/training.entity";
-import {
-  TrainingUser,
-  Role,
-} from "../entities/training-users.entity";
+import { TrainingUser, Role } from "../entities/training-users.entity";
 
 export default class TrainingRepository {
-  constructor(private trainingRepo: Repository<Training>) {}
+	constructor(private trainingRepo: Repository<Training>) {}
 
-  // If you use findAll:
-  findAll() {
-    return this.trainingRepo.find({
-      relations: ["members", "members.user", "sessions"],
-    });
-  }
+	// If you use findAll:
+	findAll() {
+		return this.trainingRepo.find({
+			relations: ["members", "members.user", "sessions"],
+		});
+	}
 
-  findTrainingsByUser(userId: number) {
-    return this.trainingRepo
-      .createQueryBuilder("training")
-      .leftJoin("training.user", "member")
-      .where("member.user.id = :userId", { userId })
-      .getMany();
-  }
+	findTrainingsByUser(userId: number) {
+		return this.trainingRepo
+			.createQueryBuilder("training")
+			.leftJoin("training.user", "member")
+			.where("member.user.id = :userId", { userId })
+			.getMany();
+	}
 
-  async findOneById(id: number) {
-    const training = await this.trainingRepo.findOne({
-      where: { id },
-      relations: ["members", "members.user", "sessions"],
-    });
-    console.log(training);
-    if (!training) {
-      throw new Error(`Training with ID ${id} not found`);
-    }
+	async findOneById(id: number) {
+		const training = await this.trainingRepo.findOne({
+			where: { id },
+			relations: ["members", "members.user", "sessions"],
+		});
+		console.log("Trainging id : ", id, training);
+		if (!training) {
+			throw new Error(`Training with ID ${id} not found`);
+		}
 
-    return training;
-  }
+		return training;
+	}
 
-  saveTraining(trainingDto: Partial<Training>) {
-    const training = this.trainingRepo.create(trainingDto);
-    return this.trainingRepo.save(training);
-  }
+	saveTraining(trainingDto: Partial<Training>) {
+		const training = this.trainingRepo.create(trainingDto);
+		return this.trainingRepo.save(training);
+	}
 
-  updateTraining(id: number, trainingDto: Partial<Training>) {
-    return this.trainingRepo.save({ id, ...trainingDto });
-  }
+	updateTraining(id: number, trainingDto: Partial<Training>) {
+		return this.trainingRepo.save({ id, ...trainingDto });
+	}
 
-  deleteTraining(id: number) {
-    return this.trainingRepo.delete(id);
-  }
+	deleteTraining(id: number) {
+		return this.trainingRepo.delete(id);
+	}
 
-  async addMembers(
-    trainingId: number,
-    members: { userId: number; role: string }[]
-  ) {
-    const insertValues = members.map((m) => ({
-      training: { id: trainingId },
-      user: { id: m.userId },
-      role: m.role as Role,
-    }));
+	async addMembers(
+		trainingId: number,
+		members: { userId: number; role: string }[]
+	) {
+		const insertValues = members.map((m) => ({
+			training: { id: trainingId },
+			user: { id: m.userId },
+			role: m.role as Role,
+		}));
 
-    return this.trainingRepo.manager
-      .getRepository(TrainingUser)
-      .save(insertValues);
-  }
+		return this.trainingRepo.manager
+			.getRepository(TrainingUser)
+			.save(insertValues);
+	}
 
-  async removeMembers(
-    trainingId: number,
-    members: { userId: number; role: string }[]
-  ) {
-    const trainingUserRepo =
-      this.trainingRepo.manager.getRepository(TrainingUser);
+	async removeMembers(
+		trainingId: number,
+		members: { userId: number; role: string }[]
+	) {
+		const trainingUserRepo =
+			this.trainingRepo.manager.getRepository(TrainingUser);
 
-    for (const m of members) {
-      await trainingUserRepo.delete({
-        training: { id: trainingId },
-        user: { id: m.userId },
-        role: m.role as Role,
-      });
-    }
+		for (const m of members) {
+			await trainingUserRepo.delete({
+				training: { id: trainingId },
+				user: { id: m.userId },
+				role: m.role as Role,
+			});
+		}
 
-    return { message: "Members removed" };
-  }
-  async findProgramsByUserId(userId: number) {
-    return this.trainingRepo.find({
-      relations: ["members", "members.user", "sessions"],
-      where: {
-        members: {
-          user: { id: userId },
-        },
-      },
-    });
-  }
+		return { message: "Members removed" };
+	}
+	async findProgramsByUserId(userId: number) {
+		return this.trainingRepo.find({
+			relations: ["members", "members.user", "sessions"],
+			where: {
+				members: {
+					user: { id: userId },
+				},
+			},
+		});
+	}
 }
--- frontend/src/pages/adminDashboard/AdminDashboard.tsx
+++ frontend/src/pages/adminDashboard/AdminDashboard.tsx
@@ -1,61 +1,67 @@
 import { useNavigate, useParams } from "react-router-dom";
 import DashboardCardList from "../../components/dashboardCardList/DashboardCardList";
 import EventList, {
-    formatTrainingList,
+	formatTrainingList,
 } from "../../components/eventList/EventList";
 import Layout from "../../components/layout/Layout";
 import { useGetUserDashboardDataQuery } from "../../api-service/user/user.api";
-import { useGetTrainingListQuery } from "../../api-service/training/training.api";
 
 const AdminDashboard = () => {
-    const navigate = useNavigate();
-    const { userId } = useParams();
-    const { data: userDashboardData, isLoading: isCardDataLoading } = useGetUserDashboardDataQuery(
-        { id: userId }
-    );
-    const { data: trainingList, isLoading } = useGetTrainingListQuery({});
-    const progress: number =
-        Number(((userDashboardData?.upcomingPrograms.length /
-            userDashboardData?.totalPrograms.length) *
-        100).toFixed(0));
-    return (
-        <Layout title="Admin Dashboard" isLoading={isCardDataLoading || isLoading}>
-            <div className="flex flex-col items-center justify-center gap-10 p-5">
-                <DashboardCardList
-                    data={[
-                        {
-                            label: "Total Programs",
-                            value: trainingList?.length,
-                        },
-                        {
-                            label: "Todays Session",
-                            value: userDashboardData?.todaysSessions.length + 3,
-                        },
-                        {
-                            label: "Completed Programs",
-                            value: userDashboardData?.completedPrograms.length + 4,
-                        },
-                        {
-                            label: "Upcoming Sessions",
-                            value: userDashboardData?.upcomingSessions.length + 2,
-                        },
-                        {
-                            label: "Program Stats",
-                            value: progress,
-                            type: "Progress",
-                        },
-                    ]}
-                />
-                <EventList
-                    isAdmin={true}
-                    heading="Trainings"
-                    showCreateButton={true}
-                    onCreateClick={() => navigate("/training/create")}
-                    data={formatTrainingList(trainingList)}
-                />
-            </div>
-        </Layout>
-    );
+	const navigate = useNavigate();
+	const { userId } = useParams();
+	const { data: userDashboardData, isLoading: isCardDataLoading } =
+		useGetUserDashboardDataQuery({ id: userId });
+	// const { data: trainingList, isLoading } = useGetTrainingListQuery({});
+	const progress: number = Number(
+		(
+			(userDashboardData?.upcomingPrograms.length /
+				userDashboardData?.totalPrograms.length) *
+			100
+		).toFixed(0)
+	);
+	return (
+		<Layout
+			title="Admin Dashboard"
+			isLoading={
+				isCardDataLoading || isCardDataLoading || !userDashboardData
+			}
+		>
+			<div className="flex flex-col items-center justify-center gap-10 p-5">
+				<DashboardCardList
+					data={[
+						{
+							label: "Total Programs",
+							value: userDashboardData?.totalPrograms.length,
+						},
+						{
+							label: "Todays Session",
+							value: userDashboardData?.todaysSessions.length,
+						},
+						{
+							label: "Completed Programs",
+							value: userDashboardData?.completedPrograms.length,
+						},
+						{
+							label: "Upcoming Sessions",
+							value: userDashboardData?.upcomingSessions.length,
+						},
+						{
+							label: "Program Stats",
+							value: progress,
+							type: "Progress",
+						},
+					]}
+				/>
+				<EventList
+					isAdmin={true}
+					heading="Trainings"
+					showCreateButton={true}
+					onCreateClick={() => navigate("/training/create")}
+					data={formatTrainingList(userDashboardData?.totalPrograms)}
+				/>
+			</div>
+		</Layout>
+	);
 };
 
 export default AdminDashboard;
